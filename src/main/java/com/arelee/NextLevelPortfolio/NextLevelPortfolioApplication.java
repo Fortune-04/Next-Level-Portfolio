0c30b0986ad503452f6d4f069153369af41d3602
Fortune-04 <wif180006@siswa.um.edu.my>
2022-09-12 19:09:35 +0800

    first commit

---
+++ src/main/java/com/arelee/NextLevelPortfolio/NextLevelPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.arelee.NextLevelPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextLevelPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextLevelPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arelee/NextLevelPortfolio/NextLevelPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arelee.NextLevelPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextLevelPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
